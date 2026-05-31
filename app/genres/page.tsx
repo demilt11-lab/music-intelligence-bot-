5550944240935f4cadfa11456031a8e69c7f3cbc
demilt11-lab <demilt11@gmail.com>
2026-05-30 20:03:59 -0400

    Create page.tsx

---
+++ app/genres/page.tsx
@@ -0,0 +1,17 @@
+// app/genres/page.tsx
+'use client';
+
+import React from 'react';
+import { PageShell } from '@/components/ui/PageShell';
+import { GenreBreakoutTable } from '@/components/genres/GenreBreakoutTable';
+
+export default function GenresPage() {
+  return (
+    <PageShell
+      title="Genre Breakout Radar"
+      description="See which genres are heating up internationally and in the US, based on UGC, playlists, and radio."
+    >
+      <GenreBreakoutTable />
+    </PageShell>
+  );
+}
