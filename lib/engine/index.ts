bb886286713961619dbeabbc4a02020cc7142a3c
demilt11-lab <demilt11@gmail.com>
2026-06-09 13:12:16 -0400

    Update index.ts

--- lib/engine/index.ts
+++ lib/engine/index.ts
@@ -1,6 +1,6 @@
 // lib/engine/index.ts
 // NOV8TE proprietary scouting engine entrypoint
 
-export * as ScoutSources from '@/lib/talentScout/sources';
-export * as ScoutScore from '@/lib/talentScout/score';
-export * as ScoutGenres from '@/lib/talentScout/genres';
+export * as ScoutSources from '@/lib/talentScout/sources'
+export * as ScoutScore from '@/lib/talentScout/score'
+export * as ScoutGenres from '@/lib/talentScout/genres'
